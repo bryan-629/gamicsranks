b0d4763dc46ca80d2b3f85df397b66d4a131cf5c
Bryan <cruzbryan629@gmail.com>
2023-07-21 19:32:42 +0200

    solucion

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ import { AuthProvider } from '../../Context/AuthProvider';
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+  <Component {...pageProps} />
+  </AuthProvider>
   )
-}
+  }
