edca6909d0959fbe335660448f3cbae0c368109f
Bryan <cruzbryan629@gmail.com>
2023-07-22 13:27:08 +0200

    cambios en el logeo, redirect

--- Context/AuthProvider.js
+++ Context/AuthProvider.js
@@ -1,10 +1,8 @@
-// useAuthentication.js
-import { db, app } from '@/firebase';
+import { db, } from '@/firebase';
 import { createContext, useContext, useEffect, useState } from 'react';
-import { getAuth, signInWithRedirect, GoogleAuthProvider, getRedirectResult, signOut } from 'firebase/auth';
-import { useRouter } from 'next/router';
+import { getAuth, signInWithRedirect, signOut, GoogleAuthProvider, onAuthStateChanged } from 'firebase/auth';
 import useApi from '@/hooks/useApi';
-
+import { useRouter } from 'next/router';
 const AuthContext = createContext();
 
 export const useAuthentication = () => {
@@ -12,18 +10,16 @@ export const useAuthentication = () => {
 };
 
 const useAuthenticationHook = () => {
-  const { data: dataSaveNewID, isLoading: isLoadingSaveNewID, error:  saveNewIDError, fetchData:saveNewID } = useApi();
-  const auth = getAuth(app);
-  const { data, isLoading, error, fetchData } = useApi();
+  const auth = getAuth();
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const googleProvider = new GoogleAuthProvider();
+  const { data: dataSaveNewID, isLoading: isLoadingSaveNewID, error: saveNewIDError, fetchData: saveNewID } = useApi();
   const [showIdModal, setShowIdModal] = useState(false);
-  const [isLoadingLoginUser, setIsLoadingLoginUser] = useState(false);
+  const [isLoadingLoginUser, setIsLoadingLoginUser] = useState(true);
   const router = useRouter();
 
-  const googleProvider = new GoogleAuthProvider();
-
   const signInWithGoogle = async () => {
-    setIsLoadingLoginUser(true);
     try {
       await signInWithRedirect(auth, googleProvider);
     } catch (error) {
@@ -35,69 +31,24 @@ const useAuthenticationHook = () => {
     try {
       await signOut(auth);
       setUser(null);
-      router.push('/');
     } catch (error) {
       console.error('Error al cerrar sesión:', error.message);
     }
   };
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(async (userChanged) => {
+    const unsubscribe = onAuthStateChanged(auth, (userChanged) => {
       if (userChanged) {
-        const form = {
-          "displayname": userChanged.displayName,
-          "email": userChanged.email,
-          "token": userChanged.stsTokenManager.accessToken,
-          "photo_url": userChanged.photoURL,
-          "emailVerified": userChanged.emailVerified,
-          "uid": userChanged.uid
-        };
-
-        const response = await fetchData(process.env.NEXT_PUBLIC_API_URL + 'login.php', 'POST', form);
-        if (response[0]) {
-          setUser(response[0]);
-          if (response[0].id === '') {
-            setShowIdModal(true);
-          }
+        console.log(userChanged)
       }
-        }
-        
-      setIsLoadingLoginUser(false);
+      setUser(userChanged);
+      setIsLoading(false);
     });
-
-    // Verificar si se completó el inicio de sesión con Google redirect
-    const checkRedirectResult = async () => {
-      try {
-        const result = await getRedirectResult(auth);
-        if (result.user) {
-          setUser(result.user);
-        }
-      } catch (error) {
-        console.error('Error al obtener el resultado de la redirección de Google:', error.message);
-      }
-    };
-
-    checkRedirectResult();
 
     return () => unsubscribe();
   }, []);
 
-  const changeId = async (newId) => {
-    const sendData = {
-      "id": encodeURI(newId.toUpperCase()),
-      "uid": user.uid
-    };
-    await saveNewID(process.env.NEXT_PUBLIC_API_URL + 'saveNewId.php', 'POST', sendData).then((response) => {
-      setUser({...user, "id": newId.toUpperCase()});
-      console.log(user);
-    });
-  };
-
-  const getUser = () => {
-    return user;
-  };
-
-  return { user, showIdModal, isLoadingLoginUser, signInWithGoogle, signOutUser, getUser, setShowIdModal, changeId };
+  return { user, isLoading, signInWithGoogle, signOutUser };
 };
 
 export const AuthProvider = ({ children }) => {
