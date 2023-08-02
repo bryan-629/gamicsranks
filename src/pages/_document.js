cb51d04136a44e13ff5759295df4d42ff35cb711
Bryan <cruzbryan629@gmail.com>
2023-08-02 13:36:03 +0200

    Cambio de color de la app de descarga

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="application-name" content="Gamics" />
-        <meta name="theme-color" content="#FF4545" />
+        <meta name="theme-color" content="#212529" />
         <meta name ='viewport' content = 'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
         <link rel="apple-touch-icon" href="/icon-192x192.png" />
         <link rel="manifest" href="/manifest.json" />
