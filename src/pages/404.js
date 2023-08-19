ea884fe5fa11a12c247c0302a911a7e466a3349a
Bryan <cruzbryan629@gmail.com>
2023-08-19 12:35:48 +0200

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import CustomNavbar from '@/components/CustomNavbar'
 function noExisteUsuario404() {
   return (
-    <div>noExisteUsuario404</div>
+    <>
+    <CustomNavbar></CustomNavbar>
+    <div className='min-vh-100 bg-dark text-white d-flex justify-content-center containter-fluid py-4'>
+      <h1 className='font-bebas'>El Usuario no existe</h1>
+    </div>
+    </>
   )
 }
 
